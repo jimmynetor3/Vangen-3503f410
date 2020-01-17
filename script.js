baae4d84a64c3faeb12d99b26c2c69a565c1e888
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-01-17 13:38:22 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,63 @@
+function gooiButton() {
+    try {
+        bal.gooi();
+    } catch (error){
+        document.getElementById("errormessage").innerText = "das de verkeerde kant";
+    }
+}
+
+function vangButton() {
+    try {
+        bal.vang();
+    } catch (error) {
+        document.getElementById("errormessage").innerText = "das de verkeerde kant";
+    }
+}
+function resetButton() {
+    bal.reset();
+}
+
+
+var bal = {
+    canvasBal: constructBal(),
+    balPositie: "links",
+
+    gooi: function () {
+        if (this.balPositie !== "links") {
+            throw Error("bal in verkeerde positie")
+        }
+        this.draw(300, 50);
+        this.balPositie = "midden";
+    },
+
+    vang: function () {
+        if (this.balPositie !== "midden") {
+            throw Error("bal in verkeerde positie")
+        }
+        this.draw(500, 250);
+        this.balPositie = "rechts";
+    },
+
+    reset: function () {
+        this.draw(100, 250);
+        this.balPositie = "links";
+    },
+
+    draw: function (x, y) {
+        this.canvasBal.clearRect(0, 0, 600, 300);
+        this.canvasBal.beginPath();
+        this.canvasBal.arc(x, y, 50, 0, 2 * Math.PI);
+        this.canvasBal.closePath();
+        this.canvasBal.fill();
+    }
+}
+
+function constructBal() {
+    let bal = document.getElementById("bal").getContext("2d");
+    bal.fillStyle = "red";
+    bal.beginPath();
+    bal.arc(100, 250, 50, 0, 2 * Math.PI);
+    bal.closePath();
+    bal.fill();
+    return bal
+}
